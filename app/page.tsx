babd7871fe9422286a15c3446f08c2b4abb61474
odmal88 <odmal@me.com>
2026-05-03 21:26:20 +0300

    Tune home page background

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      <main>
+      <main className="bg-[#eae4f4]">
         <HeroSection />
         <FeaturedWorks />
         <AboutPreview />
